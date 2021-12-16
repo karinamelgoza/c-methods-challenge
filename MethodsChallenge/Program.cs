0620b44095f4553e52030b805b4a3be484d5ce58
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-16 13:44:41 -0700

    Adds solution for mthods challenge

--- MethodsChallenge/Program.cs
+++ MethodsChallenge/Program.cs
@@ -6,7 +6,18 @@ namespace MethodsChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string friend1 = "sam";
+            string friend2 = "bob";
+            string friend3 = "tim";
+
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+        }
+
+        public static void GreetFriend(string myFriend)
+        {
+            Console.WriteLine($"hi {myFriend}, my friend!");
         }
     }
 }
