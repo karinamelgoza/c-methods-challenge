6a1ae100940be41290d198a03dfed083208744da
Karina Melgoza <kmelgoza08@gmail.com>
2021-12-16 13:34:17 -0700

    initial commit

---
+++ MethodsChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
